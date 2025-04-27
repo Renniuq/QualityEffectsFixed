3b716ae30e2e56ea129d664fb9e0f241a9e4ccae
Quinn Melville <quinn@melville.ws>
2025-04-27 01:56:36 -0600

    Update control.lua
    Working on making relevant machine information be carried over to the new machine.
    Needs any testing at all still.
    If it works - Made Module Request, circuit connections, recipe, and conditions transfer.

--- control.lua
+++ control.lua
@@ -14,6 +14,22 @@ local function check_entity(entity_name)
     return false
 end
 
+-- lifted from BigContainersUPSPlus by Taurunti (https://mods.factorio.com/mod/BigContainersUPSPlus)
+local function copy_circuit_connections(old, new)
+  -- https://lua-api.factorio.com/stable/classes/LuaEntity.html#get_wire_connectors
+  local connections = old.get_wire_connectors()
+  --if (not connections) then return end
+
+  for wire_id, luaWireConnector in pairs(connections) do
+    local newLuaWireConnector = new.get_wire_connector(wire_id, true)
+
+    for _, connection in ipairs(luaWireConnector.connections) do
+      -- https://lua-api.factorio.com/stable/classes/LuaWireConnector.html#connect_to
+      newLuaWireConnector.connect_to(connection.target)
+    end
+  end
+end
+
 local on_built = function (data)
     local entity = data.entity
     if not entity.quality then return end
@@ -32,8 +48,30 @@ local on_built = function (data)
         move_stuck_players = true,
         spill=true,
     }
-    
+
+    -- lifted from Factory Levels by sensenmann (https://mods.factorio.com/mod/factory-levels)
+    local existing_requests = surface.find_entity("item-request-proxy", entity.position)
+	if existing_requests then
+		-- Module requests do not survive the machine being replaced.  Preserve them before the machine is replaced.
+		item_requests = {}
+		for module_name, count in pairs(existing_requests.item_requests) do
+			item_requests[module_name] = count
+		end
+		if next(item_requests, nil) == nil then
+			item_requests = nil
+		end
+	end
+
     local new_entity = surface.create_entity(info)
+    copy_circuit_connections(entity, new_entity)
+    new_entity.copy_settings(entity)
+    if item_requests then
+    	surface.create_entity({ name = "item-request-proxy",
+								position = created.position,
+								force = created.force,
+								target = created,
+								item-request-proxy = item_requests })
+	end
     -- new_entity.set_recipe(entity.get_recipe())
     entity.destroy()
 end
